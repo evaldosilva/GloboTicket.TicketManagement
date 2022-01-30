71476ed357e29e8eca8e9ad6d1a23f6134eafef3
Evaldo Gomes Silva <evaldogomes@gmail.com>
2022-01-30 16:10:06 -0300

    Add project files.

---
+++ GloboTicket.TicketManagement.Infrastructure/FileExport/CsvExporter.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace GloboTicket.TicketManagement.Infrastructure
+{
+    public class CsvExporter : ICsvExporter
+    {
+        public byte[] ExportEventsToCsv(List<EventExportDto> eventExportDtos)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter, CultureInfo.CurrentUICulture);
+                csvWriter.WriteRecords(eventExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# ps-aspnetcore-architecture
+
+Architecting ASP.NET Core Applications: Best Practices
+by Gill Cleeren
+
+Creating a testable and maintainable application in .NET Core requires a solid foundation. This course will teach you best practices for creating an architecture for your ASP.NET Core applications.
+
+https://app.pluralsight.com/library/courses/architecting-asp-dot-net-core-applications-best-practices/table-of-contents
+
